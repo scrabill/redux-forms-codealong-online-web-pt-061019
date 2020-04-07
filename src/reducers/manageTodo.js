c8549c8278c825fa27bbf62d06a7f9c94c07fae8
scrabill <shannon.crabill@gmail.com>
2020-04-07 16:35:02 -0400

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -2,7 +2,17 @@ export default function manageTodo(state = {
   todos: [],
 }, action) {
 
-  console.log("reducer received this action:", action);
+  switch (action.type) {
+    case 'ADD_TODO':
+        console.log("reducer received this action:", action);
+
+        return { todos: state.todos.concat(action.payload.text) };
+    default:
+      return state
+
+  }
+
+
 
   return state;
 }
